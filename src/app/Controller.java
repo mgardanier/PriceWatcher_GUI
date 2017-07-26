8450534b8902b2dbedf6f98997699d08d2976a1a
Michael <michael.gardanier@salesforce.com>
2017-07-26 11:15:54 -0700

    added test javafx frame

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,5 +1,39 @@
 package app;
 
+import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextField inputText;
+
+    @FXML
+    private TextArea outputField;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ResourceBundle resources;
+
+    public Controller(){
+
+    }
+
+    @FXML
+    private void initialize(){
+
+    }
+
+    @FXML
+    private void printOutput(){
+        outputField.setText(inputText.getText());
+    }
 
 }
--- src/app/Main.java
+++ src/app/Main.java
@@ -4,15 +4,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader();
+        FileInputStream fxmlStream = new FileInputStream("testLayout.fxml");
+
+        AnchorPane root = (AnchorPane) loader.load(fxmlStream);
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Test Example");
         primaryStage.show();
     }
 
